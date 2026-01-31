0e73c5e5c7db52e1739a4d25b0e6ee7463edb897
Meoah <78709303+Meoah@users.noreply.github.com>
2026-01-30 19:40:55 -0800

    State Machine Start

--- Cyberlink/scripts/managers/game_manager.gd
+++ Cyberlink/scripts/managers/game_manager.gd
@@ -1,5 +1,12 @@
 extends Node
 
+## States
+var play_state : PlayState
+var pause_state : PauseState
+var main_menu_state : MainMenuState
+
+var state_machine : StateMachine
+
 func _ready() -> void:
 	# Check to see if the current scene is the default. If so, kill it.
 	# This script is attached to an autoload, so it can't also be the initial
@@ -10,3 +17,20 @@ func _ready() -> void:
 		default.queue_free()
 	
 	var _main_scene = get_tree().current_scene
+	
+	_setup_state_machine()
+
+func _setup_state_machine() -> void:
+	var transitions : Dictionary = {
+		MainMenuState.STATE_NAME : [PlayState.STATE_NAME],
+		PlayState.STATE_NAME : [MainMenuState.STATE_NAME],
+		PauseState.STATE_NAME : [PlayState.STATE_NAME],
+	}
+	
+	state_machine = StateMachine.new("game_state", transitions)
+	
+	main_menu_state = MainMenuState.new(state_machine)
+	play_state = PlayState.new(state_machine)
+	pause_state = PauseState.new(state_machine)
+	
+	state_machine.transition_to(main_menu_state)
