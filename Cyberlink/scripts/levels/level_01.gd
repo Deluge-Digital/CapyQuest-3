98ea78c1b0b58391ed411c347d3f185bbd3e1e02
DarvenAcevedo <darven.acevedo2021@gmail.com>
2026-02-01 12:34:31 -0800

    Updating Levels

--- Cyberlink/scripts/levels/level_01.gd
+++ Cyberlink/scripts/levels/level_01.gd
@@ -5,17 +5,3 @@ extends BaseLevel
 
 func _ready() -> void:
 	pass
-
-func wincheck():
-	if $Ratmouse.position == $Cheese.position:
-		$Ratmouse/AnimationPlayer.play("Movement Anim")
-		$Cheese.victory()
-		print("youwin!!")
-
-func _input(event: InputEvent) -> void:
-	wincheck()
-	
-
-
-func _on_animated_sprite_3d_animation_finished() -> void:
-	$Camera3D/AnimatedSprite3D.pause()
