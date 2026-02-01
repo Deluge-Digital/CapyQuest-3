6c80c5b475d2abef0f403a1a345ffbd5e32d0bbd
DarvenAcevedo <darven.acevedo2021@gmail.com>
2026-02-01 01:59:29 -0800

    Gate Refined and fixed

--- Cyberlink/scripts/props/gate.gd
+++ Cyberlink/scripts/props/gate.gd
@@ -2,46 +2,58 @@ extends BaseProp
 class_name Gate
 
 @export var tile_color: color_enum.TileColor = color_enum.TileColor.NONE
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
-
+@onready var left_door: MeshInstance3D = $Gate2/LeftDoor 
+@onready var right_door: MeshInstance3D = $Gate2/RightDoor 
 
 
 func set_color():
 	if tile_color == color_enum.TileColor.RED:
 		var material : StandardMaterial3D = null
+		var material2 : StandardMaterial3D = null
 		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
+			material = left_door.mesh.surface_get_material(0).duplicate()
+			material2 = right_door.mesh.surface_get_material(0).duplicate()
+			left_door.mesh.surface_set_material(0, material)
+			right_door.mesh.surface_set_material(0, material)
+		material2.albedo_color = Color(1.0, 0.0, 0.0, 1.0)
 		material.albedo_color = Color(1.0, 0.0, 0.0, 1.0)
 	elif tile_color == color_enum.TileColor.BLUE:
 		var material : StandardMaterial3D = null
+		var material2 : StandardMaterial3D = null
 		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
-		material.albedo_color = Color(0.0, 0.25, 1.0, 1.0)
+			material = left_door.mesh.surface_get_material(0).duplicate()
+			material2 = right_door.mesh.surface_get_material(0).duplicate()
+			left_door.mesh.surface_set_material(0, material)
+			right_door.mesh.surface_set_material(0, material)
+		material2.albedo_color = Color(0.017, 0.0, 1.0, 1.0)
+		material.albedo_color = Color(0.017, 0.0, 1.0, 1.0)
 	elif tile_color == color_enum.TileColor.GREEN:
 		var material : StandardMaterial3D = null
+		var material2 : StandardMaterial3D = null
 		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
-		material.albedo_color = Color(0.017, 1.0, 0.0, 1.0)
+			material = left_door.mesh.surface_get_material(0).duplicate()
+			material2 = right_door.mesh.surface_get_material(0).duplicate()
+			left_door.mesh.surface_set_material(0, material)
+			right_door.mesh.surface_set_material(0, material)
+		material2.albedo_color = Color(0.117, 1.0, 0.0, 1.0)
+		material.albedo_color = Color(0.117, 1.0, 0.0, 1.0)
 	elif tile_color == color_enum.TileColor.BLUE_RED:
 		var material : StandardMaterial3D = null
 		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
+			material = left_door.mesh.surface_get_material(0).duplicate()
+			left_door.mesh.surface_set_material(0, material)
 		material.albedo_color = Color(0.967, 0.0, 1.0, 1.0)
 	elif tile_color == color_enum.TileColor.BLUE_GREEN:
 		var material : StandardMaterial3D = null
 		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
+			material = left_door.mesh.surface_get_material(0).duplicate()
+			left_door.mesh.surface_set_material(0, material)
 		material.albedo_color = Color(0.0, 1.0, 0.95, 1.0)
 	elif tile_color == color_enum.TileColor.GREEN_RED:
 		var material : StandardMaterial3D = null
 		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
+			material = left_door.mesh.surface_get_material(0).duplicate()
+			left_door.mesh.surface_set_material(0, material)
 		material.albedo_color = Color(0.983, 1.0, 0.0, 1.0)
 
 func _ready() -> void:
