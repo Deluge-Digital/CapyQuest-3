0e05b72ca88067be051aae094e1b1a2ec154ce03
Meoah <liyen-liu@hotmail.com>
2026-03-07 17:17:26 -0800

    Fixed wall color. Disabled ground color for now.

--- scripts/props/ground.gd
+++ scripts/props/ground.gd
@@ -2,47 +2,27 @@ extends BaseProp
 class_name Ground
 
 
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var mesh_instance_3d: MeshInstance3D = $Floor/Floor
 
 
 func set_color():
-	if tile_color == color_enum.TileColor.RED:
-		var material : StandardMaterial3D = null
-		if material == null:
+	var material : StandardMaterial3D = null
+	if material == null:
 			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
+	if tile_color == color_enum.TileColor.RED:
 		material.albedo_color = Color(1.0, 0.0, 0.0, 1.0)
 	elif tile_color == color_enum.TileColor.BLUE:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
 		material.albedo_color = Color(0.0, 0.25, 1.0, 1.0)
 	elif tile_color == color_enum.TileColor.GREEN:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
 		material.albedo_color = Color(0.017, 1.0, 0.0, 1.0)
 	elif tile_color == color_enum.TileColor.BLUE_RED:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
 		material.albedo_color = Color(0.967, 0.0, 1.0, 1.0)
 	elif tile_color == color_enum.TileColor.BLUE_GREEN:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
 		material.albedo_color = Color(0.0, 1.0, 0.95, 1.0)
 	elif tile_color == color_enum.TileColor.GREEN_RED:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
 		material.albedo_color = Color(0.983, 1.0, 0.0, 1.0)
+	mesh_instance_3d.mesh.surface_set_material(0, material)
 
 
 func _ready() -> void:
-	set_color()
+	pass #set_color()
--- scripts/props/wall.gd
+++ scripts/props/wall.gd
@@ -5,42 +5,22 @@ class_name Wall
 
 
 func set_color():
+	var material : StandardMaterial3D = null
+	if material == null:
+			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
 	if tile_color == color_enum.TileColor.RED:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
-		material.albedo_color = Color(1.0, 0.0, 0.0, 1.0)
+		material.albedo_color = Color(1.0, 0.0, 0.0, 0.5)
 	elif tile_color == color_enum.TileColor.BLUE:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
-		material.albedo_color = Color(0.0, 0.25, 1.0, 1.0)
+		material.albedo_color = Color(0.0, 0.25, 1.0, 0.5)
 	elif tile_color == color_enum.TileColor.GREEN:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
-		material.albedo_color = Color(0.017, 1.0, 0.0, 1.0)
+		material.albedo_color = Color(0.017, 1.0, 0.0, 0.5)
 	elif tile_color == color_enum.TileColor.BLUE_RED:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
-		material.albedo_color = Color(0.967, 0.0, 1.0, 1.0)
+		material.albedo_color = Color(0.967, 0.0, 1.0, 0.5)
 	elif tile_color == color_enum.TileColor.BLUE_GREEN:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
-		material.albedo_color = Color(0.0, 1.0, 0.95, 1.0)
+		material.albedo_color = Color(0.0, 1.0, 0.95, 0.5)
 	elif tile_color == color_enum.TileColor.GREEN_RED:
-		var material : StandardMaterial3D = null
-		if material == null:
-			material = mesh_instance_3d.mesh.surface_get_material(0).duplicate()
-			mesh_instance_3d.mesh.surface_set_material(0, material)
-		material.albedo_color = Color(0.983, 1.0, 0.0, 1.0)
+		material.albedo_color = Color(0.983, 1.0, 0.0, 0.5)
+	mesh_instance_3d.mesh.surface_set_material(0, material)
 
 func get_color() -> color_enum.TileColor:
 	return tile_color
